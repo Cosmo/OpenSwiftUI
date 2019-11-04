f5924fbd65dc97fec5d892864e64254e08f05c14
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-11-04 01:12:42 +0100

    Update Image.swift

--- Sources/OpenSwiftUI/Views/Image.swift
+++ Sources/OpenSwiftUI/Views/Image.swift
@@ -1,13 +1,3 @@
-public struct ImageData: Equatable {
-    public var bytes: [UInt8]
-    public var size: Size
-    
-    public init(bytes: [UInt8], size: Size) {
-        self.bytes = bytes
-        self.size = size
-    }
-}
-
 public struct Image: View, Equatable {
     public typealias Body = Never
     
@@ -21,5 +11,18 @@ public struct Image: View, Equatable {
 extension Image {
     public var body: Never {
         fatalError()
+    }
+}
+
+// This actually does not belong in OpenSwiftUI
+// But for now will stay here, until there is
+// a better way to pass image data to Image().
+public struct ImageData: Equatable {
+    public var bytes: [UInt8]
+    public var size: Size
+    
+    public init(bytes: [UInt8], size: Size) {
+        self.bytes = bytes
+        self.size = size
     }
 }
