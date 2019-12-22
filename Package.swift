3336cba495b2694b29ac7a9eaa32086bc360fff7
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-12-22 19:49:51 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,13 +17,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Cosmo/CoreGraphicsShim.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "OpenSwiftUI",
-            dependencies: []),
+            dependencies: ["CoreGraphicsShim"]),
         .testTarget(
             name: "OpenSwiftUITests",
             dependencies: ["OpenSwiftUI"]),
