e35adee88e5e0a8096c36c04b49282f8a261e153
Mathias Quintero <me@quintero.io>
2020-05-21 21:31:13 +0200

    Adding tests for AnyView initializer

--- Tests/OpenSwiftUITests/OpenSwiftUITests.swift
+++ Tests/OpenSwiftUITests/OpenSwiftUITests.swift
@@ -8,15 +8,35 @@ final class OpenSwiftUITests: XCTestCase {
             Text("World")
         }
         
-        let body = HStack {
-            if true {
-                Text("Hello")
-            }
-            Text("World")
+    }
+
+    func testAnyViewFromValueWithInDoesNotYieldView() {
+        let anyView = AnyView(_fromValue: 42)
+        XCTAssertNil(anyView)
+    }
+
+    func testAnyViewFromValueWithTextYieldsAnyView() {
+        let expectedText = "Hello"
+        let value: Any = Text(expectedText)
+        let anyView = AnyView(_fromValue: value)
+        XCTAssertNotNil(anyView)
+
+        guard let storage = anyView?._storage as? AnyViewStorage<Text> else {
+            XCTFail("View storage is not an AnyViewStorage of Text")
+            return
+        }
+
+        switch storage._view._storage {
+        case .verbatim(let string):
+            XCTAssertEqual(string, expectedText)
+        case .anyTextStorage(let storage):
+            XCTAssertEqual(storage.storage, expectedText)
         }
     }
 
     static var allTests = [
         ("testExample", testExample),
+        ("testAnyViewFromValueWithInDoesNotYieldView", testAnyViewFromValueWithInDoesNotYieldView),
+        ("testAnyViewFromValueWithTextYieldsAnyView", testAnyViewFromValueWithTextYieldsAnyView),
     ]
 }
